92e839e473da4261de62b125f074c30400f45a1d
Sumeya Hirsi <sumeyahirsi5@gmail.com>
2025-11-18 16:21:12 +0300

    add file

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,17 @@
-#!/usr/bin/env python3
-
 class Dog:
-    # Class body goes here
+    # Instance Method Definition provided in the lab
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+    # Task: Add an instance method sit()
+    def sit(self):
+        print("The dog is sitting.")
+
+
+# Example usage provided in the lab:
+# fido = Dog()
+# fido.bark()
+# # Woof!
+# snoopy = Dog()
+# snoopy.bark()
+# # Woof!
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,16 @@
-#!/usr/bin/env python3
-
 class Person:
-    # Class body goes here
+  # Task: Add an instance method talk()
+  def talk(self):
+    print("Hello World!")
 
-    #Instance method definition
-    pass
+  # Task: Add an instance method walk()
+  def walk(self):
+    print("The person is walking.")
+
+# Example usage:
+# alice = Person()
+# alice.talk()
+# # Hello World!
+# bob = Person()
+# bob.walk()
+# # The person is walking.
